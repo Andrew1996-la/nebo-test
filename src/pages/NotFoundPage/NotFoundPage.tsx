20f8a899496b6fcc038d1279eb4e20b12119dc79
Andrew <asoborev@mail.ru>
2024-06-13 14:53:30 +0300

    designed 404 pages

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,7 +1,23 @@
-const NotFoundPage = () => {
-    return (
-        <>not found page</>
-    )
-}
+import { useNavigate } from 'react-router-dom';
+import Button from '../../components/Button/Button';
+import s from './notFoundPage.module.css';
 
-export default NotFoundPage
+const NotFoundPage = () => {
+    const navigate = useNavigate();
+    return (
+        <div className={s.notFound}>
+            <div>
+                <h1> 404: Страница ушла на тёмную сторону</h1>
+                <div>
+                    Темная сторона поглотила эту страницу. Вернитесь на главную,
+                    чтобы найти свет.
+                </div>
+            </div>
+            <Button onClick={() => navigate(-1)} className={s.button}>
+                На светлую
+            </Button>
+        </div>
+    );
+};
+
+export default NotFoundPage;
